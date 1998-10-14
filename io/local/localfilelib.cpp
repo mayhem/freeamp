ad4dfa6fa056e409a7d3c4c180074f96f694173f
elrod <elrod>
1998-10-14 08:52:09 +0000

    * created DLL wrappers for the c++ objects

---
+++ io/local/localfilelib.cpp
@@ -0,0 +1,75 @@
+/*____________________________________________________________________________
+	
+	FreeAmp - The Free MP3 Player
+	Portions Copyright (C) 1998 GoodNoise
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+	
+	$Id: localfilelib.cpp,v 1.1 1998/10/14 08:52:09 elrod Exp $
+____________________________________________________________________________*/
+
+/* project headers */
+#include "pmilib.h"
+#include "localfileinput.h"
+
+void Initialize(PMIRef ref)
+{
+    if(ref)
+    {
+        PhysicalMediaInput* pmi = new LocalFileInput;
+        ref->ref = pmi;
+
+        ref->Read = Read;
+        ref->Seek = Seek;
+        ref->SetTo = SetTo;
+        ref->Close = Close;
+        ref->Url = Url;
+    }
+}
+
+int32 Read(PMIRef ref, void* buf, size_t len)
+{
+    PhysicalMediaInput* pmi = (PhysicalMediaInput*)ref;
+
+    return pmi->Read(buf, len);
+}
+
+int32 Seek(PMIRef ref, int32 offset, int32 origin)
+{
+    PhysicalMediaInput* pmi = (PhysicalMediaInput*)ref;
+
+    return pmi->Seek(offset, origin);
+}
+
+bool SetTo(PMIRef ref, char* url)
+{
+    PhysicalMediaInput* pmi = (PhysicalMediaInput*)ref;
+
+    return pmi->SetTo(url);
+}
+
+bool Close(PMIRef ref)
+{ 
+    PhysicalMediaInput* pmi = (PhysicalMediaInput*)ref;
+
+    return pmi->Close();
+}
+
+const char* Url(PMIRef ref)
+{
+    PhysicalMediaInput* pmi = (PhysicalMediaInput*)ref;
+
+    return pmi->Url();
+}
---
+++ io/soundcard/soundcardlib.cpp
@@ -0,0 +1,75 @@
+/*____________________________________________________________________________
+	
+	FreeAmp - The Free MP3 Player
+	Portions Copyright (C) 1998 GoodNoise
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+	
+	$Id: soundcardlib.cpp,v 1.1 1998/10/14 08:52:09 elrod Exp $
+____________________________________________________________________________*/
+
+/* project headers */
+#include "pmolib.h"
+#include "soundcardpmo.h"
+
+void Initialize(PMORef ref)
+{
+    if(ref)
+    {
+        PhysicalMediaOutput* pmo = new SoundCardPMO;
+        ref->ref = pmo;
+
+        ref->Init = Init;
+        ref->Reset = Reset;
+        ref->Write = Write;
+        ref->WriteThis = WriteThis;
+        ref->Clear = Clear;
+    }
+}
+
+bool Init(PMORef ref, OutputInfo* info)
+{
+    PhysicalMediaOutput* pmo = (PhysicalMediaOutput*)ref;
+
+    return pmo->Init(info);
+}
+
+bool Reset(PMORef ref, bool user_stop)
+{
+    PhysicalMediaOutput* pmo = (PhysicalMediaOutput*)ref;
+
+    return pmo->Reset(user_stop);
+}
+
+int32 Write(PMORef ref)
+{
+    PhysicalMediaOutput* pmo = (PhysicalMediaOutput*)ref;
+
+    return pmo->Write();
+}
+
+int32 WriteThis(PMORef ref, void* buf, int32 len)
+{ 
+    PhysicalMediaOutput* pmo = (PhysicalMediaOutput*)ref;
+
+    return pmo->WriteThis(buf, len);
+}
+
+void Clear(PMORef ref)
+{
+    PhysicalMediaOutput* pmo = (PhysicalMediaOutput*)ref;
+
+    pmo->Clear();
+}

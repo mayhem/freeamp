36dd3ce7fb7577dd42bb29b91a812a74af20e672
elrod <elrod>
1998-11-09 07:48:18 +0000

    * added icons to indicate play modes * fixed region scanning bug * fixed cursor disappearing bug * fixed.... alot of bugs!

--- ui/freeamp/win32/res/resource.h
+++ ui/freeamp/win32/res/resource.h
@@ -45,13 +45,19 @@
 #define IDB_REPEAT_256                  150
 #define IDB_OPEN_256                    151
 #define IDB_COLORS                      152
+#define IDB_SHUFFLED_ICON_256           153
+#define IDB_REPEAT_ALL_ICON_256         154
+#define IDB_REPEAT_ICON                 155
+#define IDB_REPEAT_ICON_256             156
+#define IDB_SHUFFLED_ICON               157
+#define IDB_REPEAT_ALL_ICON             158
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        153
+#define _APS_NEXT_RESOURCE_VALUE        159
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101

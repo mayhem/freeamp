d04b1f2b3a7c160d97c8038710d474885a183aa2
mhw <mhw>
1999-03-03 21:10:54 +0000

    Renamed x86intel.s to x86intel.asm

--- lmc/xingmp3/src/x86intel.asm
+++ lmc/xingmp3/src/x86intel.asm
@@ -26,7 +26,7 @@
 ;	along with this program; if not, write to the Free Software
 ;	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 ;
-;	$Id: x86intel.s,v 1.1 1999/03/02 02:51:18 mhw Exp $
+;	$Id: x86intel.asm,v 1.1 1999/03/03 21:10:54 mhw Exp $
 ;
 
 BITS 32

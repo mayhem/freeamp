85c51e62442ed7fc1a754bd563a4cb4cdee3280e
elrod <elrod>
1999-03-03 09:32:35 +0000

    * updated for UI rewrite

--- ui/freeamp/win32/res/resource.h
+++ ui/freeamp/win32/res/resource.h
@@ -5,13 +5,10 @@
 #define IDI_EXE_ICON                    101
 #define IDB_DIALS                       104
 #define IDC_DIAL                        104
-#define IDB_PLAYER_MASK                 105
-#define IDB_PLAYER                      110
+#define IDB_CONTROL_MASK_MID            105
 #define IDB_MID                         111
 #define IDB_LEFT                        112
 #define IDB_RIGHT                       113
-#define IDB_PLAYER_BUTTONS              114
-#define IDB_SYSTEM_BUTTONS              115
 #define IDB_BIG_FONT                    117
 #define IDB_SMALL_FONT                  118
 #define IDB_LOGO                        119
@@ -51,13 +48,25 @@
 #define IDB_REPEAT_ICON_256             156
 #define IDB_SHUFFLED_ICON               157
 #define IDB_REPEAT_ALL_ICON             158
+#define IDB_WINDOW_MASK_MID             159
+#define IDB_BODY                        160
+#define IDB_BODY256                     161
+#define IDB_TIME_BACK                   166
+#define IDB_DRAWER_MASK                 167
+#define IDB_DRAWER                      168
+#define IDB_PLAYLIST_BODY               169
+#define IDB_PLAYLIST_CONTROL_MASK       170
+#define IDB_PANEL_BACKING_MASK          171
+#define IDB_PANEL_BACKING               172
+#define IDB_TITLE_BACK                  173
+#define IDB_SCROLLBAR                   174
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_RESOURCE_VALUE        175
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101

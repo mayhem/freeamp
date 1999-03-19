9366fe06a3e52f58c6941e07e9159605fdc7e350
elrod <elrod>
1999-03-19 04:48:10 +0000

    * added european characters to bitmap and fontwidth table, whew!

--- ui/freeamp/win32/src/textview.cpp
+++ ui/freeamp/win32/src/textview.cpp
@@ -18,7 +18,7 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$Id: textview.cpp,v 1.4 1999/03/18 20:37:32 elrod Exp $
+	$Id: textview.cpp,v 1.5 1999/03/19 04:48:10 elrod Exp $
 ____________________________________________________________________________*/
 
 /* system headers */
@@ -252,10 +252,11 @@ SetText(char* text)
     // that will hold the pre-rendered string
     for(i = 0; m_text[i]; i++)
     {
-        if(m_text[i] < 127 && m_text[i] > 31)
+        textLength += m_fontWidths[(BYTE)(m_text[i] - 32)];
+        /*if(m_text[i] < 127 && m_text[i] > 31)
             textLength += m_fontWidths[m_text[i] - 32];
         else
-            textLength += m_fontWidths[63 - 32];
+            textLength += m_fontWidths[63 - 32];*/
     }
 
     m_needToScroll = (textLength > Width());
@@ -294,7 +295,10 @@ SetText(char* text)
         int32 y;
         int32 width;
 
-        if(m_text[i] < 127 && m_text[i] > 31)
+        y = ((BYTE)(m_text[i] - 32))*m_fontHeight;
+        width = m_fontWidths[(BYTE)(m_text[i] - 32)];
+
+        /*if(m_text[i] < 127 && m_text[i] > 31)
         {
             y = (m_text[i] - 32)*m_fontHeight;
             width = m_fontWidths[m_text[i] - 32];
@@ -303,7 +307,7 @@ SetText(char* text)
         {
             y = (63 - 32)*m_fontHeight;
             width = m_fontWidths[63 - 32];
-        }
+        }*/
 
         Renderer::Copy( m_textBitmap,
                         offset, 

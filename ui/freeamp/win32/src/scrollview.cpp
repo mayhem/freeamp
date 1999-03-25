db7fefbe640b3a2e7087e243f15e67e53c21a38c
elrod <elrod>
1999-03-25 07:13:43 +0000

    * made a cast during a compare to remove a warning

--- ui/freeamp/win32/src/scrollview.cpp
+++ ui/freeamp/win32/src/scrollview.cpp
@@ -18,7 +18,7 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$Id: scrollview.cpp,v 1.6 1999/03/24 20:03:21 elrod Exp $
+	$Id: scrollview.cpp,v 1.7 1999/03/25 07:13:43 elrod Exp $
 ____________________________________________________________________________*/
 
 /* system headers */
@@ -200,7 +200,7 @@ MouseMove(int32 x, int32 y, int32 modifiers)
 
                 index += proportion;*/
 
-                if(index > m_max - m_largeStep)
+                if(index > (int32)(m_max - m_largeStep))
                     index = m_max - m_largeStep;
 
                 m_position = index;

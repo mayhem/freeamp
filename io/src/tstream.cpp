39e6500bb984278d18bbb840c73fa1861576c14b
elrod <elrod>
1999-07-28 01:40:55 +0000

    * fixed a bad cast in tstream.cpp

--- io/src/tstream.cpp
+++ io/src/tstream.cpp
@@ -19,7 +19,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
    
-   $Id: tstream.cpp,v 1.5 1999/07/27 19:25:01 robert Exp $
+   $Id: tstream.cpp,v 1.6 1999/07/28 01:40:55 elrod Exp $
 ____________________________________________________________________________*/
 
 #include <stdio.h>
@@ -261,7 +261,7 @@ void TitleStreamServer::WorkerThread(void)
       {
           iStructSize = sizeof(struct sockaddr_in);
           iRet = recvfrom(m_hHandle, buf, 255, 0, (struct sockaddr *)m_pSin, 
-                          (uint32 *)&iStructSize);
+                          &iStructSize);
       }
       else
           iRet = recv(m_hHandle, buf, 255, 0);

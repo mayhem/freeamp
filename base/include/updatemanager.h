e5df458a71388bce3974a07dddc4ad9dc70579c4
elrod <elrod>
1999-10-04 23:15:07 +0000

    * First shot at defining UpdateManager class

--- base/include/updatemanager.h
+++ base/include/updatemanager.h
@@ -18,7 +18,7 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$Id: updatemanager.h,v 1.1.2.1 1999/10/04 17:56:23 elrod Exp $
+	$Id: updatemanager.h,v 1.1.2.2 1999/10/04 23:15:07 elrod Exp $
 ____________________________________________________________________________*/
 
 #ifndef INCLUDED_UPDATE_MANAGER_H_
@@ -42,18 +42,85 @@ using namespace std;
 
 #define kInvalidIndex 0xFFFFFFFF
 
+typedef enum {
+    kUMEvent_Status,
+    kUMEvent_Progress,
+    kUMEvent_Error,
+    kUMEvent_Done
 
+} UMEventType;
+
+#define kUMProgressInfinity -1
+
+class UpdateItem;
+
+typedef struct UMEventProgressData {
+    uint32 position;
+    uint32 total;
+    UpdateItem* item;
+} UMEventProgressData;
+
+typedef struct UMEventErrorData {
+    uint32 errorCode;
+} UMEventErrorData;
+
+typedef union UMEventData {
+    UMEventProgressData progressData;
+    UMEventErrorData    errorData;
+} UMEventData;
+
+typedef struct UMEvent {
+    UMEventType type;
+    UMEventData data;
+    string eventString;
+} UMEvent;
+
+typedef bool (*UMCallBackFunction)(UMEvent* event, void* userData);
+
+class UpdateItem {
+ public:
+    virtual ~UpdateItem() { }
+    
+    void SetLocalFile(const string& path) { m_localFile = path; }
+    const string& GetLocalFile() const { return m_localFile; }
+
+    void SetCurrentFile(const string& url) { m_currentFile = url; }
+    const string& GetCurrentFile() const { return m_currentFile; }
+
+    void SetLocalFileVersion(const string& version) { m_localVersion = version; }
+    const string& GetLocalFileVersion() const { return m_localVersion; }
+
+    void SetCurrentFileVersion(const string& version) { m_currentVersion = version; }
+    const string& GetCurrentFileVersion() const { return m_currentVersion; }
+
+ protected:
+
+ private:
+    string m_localVersion;
+    string m_localFile;
+    string m_currentVersion;
+    string m_currentFile;
+};
 
 class UpdateManager {
 
  public:
-    UpdateManager(FAContext* context);
     virtual ~UpdateManager();
+    
+    virtual Error DetermineLocalVersions() { return kError_FeatureNotSupported; }
+    virtual Error RetrieveLatestVersionInfo(UMCallBackFunction function,
+                                            void* cookie);
+    virtual Error UpdateComponents(UMCallBackFunction function,
+                                   void* cookie);
 
-   
+    // Utility Functions
+    bool            IsEmpty();
+    uint32          CountItems();
+    UpdateItem*     ItemAt(uint32 index);
+	uint32          IndexOf(UpdateItem* item);
+    bool            HasItem(UpdateItem* item);
 
  protected:
-    
 
  private:
 

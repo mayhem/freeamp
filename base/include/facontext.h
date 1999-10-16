fdf2a3574f5841baffd3063fd8b95706f101a45d
ijr <ijr>
1999-10-16 21:22:30 +0000

    remove the xlib ui stuff from configure and the makefiles, set the default ui to the new freeamp.ui, fix the warning in FreeAmpTheme.cpp, make the GTKPreferenceWindow junk compile, but not yet run, some random gtkmusicbrowserui stuff, remove the stuff in ui/gtk..

--- base/include/facontext.h
+++ base/include/facontext.h
@@ -18,7 +18,7 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$Id: facontext.h,v 1.3.8.8 1999/10/13 23:07:51 robert Exp $
+	$Id: facontext.h,v 1.3.8.9 1999/10/16 21:22:30 ijr Exp $
 ____________________________________________________________________________*/
 
 #ifndef INCLUDED_FACONTEXT_H_
@@ -51,19 +51,19 @@ class FAContext
 	  minorVersion(c_minorVersion),
 	  prefs(0),
 	  log(0),
-      props(0),
-      plm(0),
-      target(0),
+          props(0),
+          plm(0),
+          target(0),
 	  browser(0),
-      downloadManager(0),
-      updateManager(0),
+          downloadManager(0),
+          updateManager(0),
+          player(0),
 #ifndef WIN32	  
 	  gtkInitialized(false),
 #endif	  
 	  argc(0),
           argv(0),	  
-	  argFlags(0),
-      player(0) { }
+	  argFlags(0) { }
     
     ~FAContext()
     {
--- base/unix/src/unixprefs.cpp
+++ base/unix/src/unixprefs.cpp
@@ -19,7 +19,7 @@
         along with this program; if not, write to the Free Software
         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
         
-        $Id: unixprefs.cpp,v 1.13.2.6 1999/10/01 15:22:33 ijr Exp $
+        $Id: unixprefs.cpp,v 1.13.2.7 1999/10/16 21:22:30 ijr Exp $
 ____________________________________________________________________________*/
 
 #include "config.h"
@@ -47,7 +47,7 @@ ____________________________________________________________________________*/
 
 // default values
 const char*  kDefaultLibraryPath = ".:~/.freeamp:" UNIX_LIBDIR "/freeamp";
-const char*  kDefaultUI = "freeamp-xlib.ui";
+const char*  kDefaultUI = "freeamp.ui";
 const char*  kDefaultTextUI = "freeampcmd.ui";
 const char*  kDefaultPMO = "soundcard.pmo";
 const char*  kDefaultALSADevice = "1:1";

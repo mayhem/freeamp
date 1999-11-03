38698cbf9f5249e9af221947afe5ae328289edb6
robert <robert>
1999-11-03 21:11:33 +0000

    I just applied Valter's theme tar patch. This may temoprarily break CVS.

--- ui/freeamp/src/ThemeZip.cpp
+++ ui/freeamp/src/ThemeZip.cpp
@@ -18,9 +18,10 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-   $Id: ThemeZip.cpp,v 1.2 1999/10/19 07:13:17 elrod Exp $
+   $Id: ThemeZip.cpp,v 1.3 1999/11/03 21:11:33 robert Exp $
 ____________________________________________________________________________*/ 
 
+#include <time.h>
 #include <stdio.h>
 
 #ifdef WIN32
@@ -52,43 +53,63 @@ ThemeZip::~ThemeZip(void)
 {
 }
 
+//
+//
+// Standard Archive Format - Standard TAR - USTAR
+//
+#define  RECORDSIZE  512
+#define  NAMSIZ      100
+#define  TUNMLEN      32
+#define  TGNMLEN      32
+
+#pragma pack(push,1)
+union tar_record {
+    char   charptr[RECORDSIZE];
+    struct _header {
+        char    name[NAMSIZ];
+        char    mode[8];
+        char    uid[8];
+        char    gid[8];
+        char    size[12];
+        char    mtime[12];
+        char    chksum[8];
+        char    typeflag;
+        char    linkname[NAMSIZ];
+        char    magic[8];
+        char    uname[TUNMLEN];
+        char    gname[TGNMLEN];
+        char    devmajor[8];
+        char    devminor[8];
+    } header;
+};
+#pragma pack(pop)
+
+// The checksum field is filled with this while the checksum is computed.
+#define    CHKBLANKS    "        "        // 8 blanks, no null 
+// The magic field is filled with this if uname and gname are valid.
+#define    TMAGIC    "ustar  "        // 7 chars and a null
+
+
 Error ThemeZip::CompressThemeZip(const string &oDestFile, 
                                  vector<string *> &oFileList)
 {
    gzFile  pOut;
    FILE   *pIn;
    char   *pBuffer;
+   tar_record TarRecord;
    string  oFile;
    vector<string *>::iterator i;
-   int     iNetworkInt, iWrite, iRead, iSize, iBlock;
+   int     iWrite, iRead, iSize, iBlock,iPadding;
 
    pOut = gzopen(oDestFile.c_str(), "wb");
    if (pOut == NULL)
        return kError_FileNoAccess;
 
-   iNetworkInt = htonl(oFileList.size());
-   if (gzwrite(pOut, (void *)&iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pOut);
-       return kError_WriteFile;
-   }
-   iNetworkInt = htonl(iMajorVersion);
-   if (gzwrite(pOut, (void *)&iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pOut);
-       return kError_WriteFile;
-   }
-   iNetworkInt = htonl(iMinorVersion);
-   if (gzwrite(pOut, (void *)&iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pOut);
-       return kError_WriteFile;
-   }
-
    pBuffer = new char[iBufferSize];
    for(i = oFileList.begin(); i != oFileList.end(); i++)
    {
        char *pPtr;
+       time_t ltime;
 
        oFile = *(*i);
        pPtr = strrchr(oFile.c_str(), DIR_MARKER);
@@ -103,35 +124,44 @@ Error ThemeZip::CompressThemeZip(const string &oDestFile,
            return kError_FileNotFound;
        }
 
-       iSize = oFile.length() + 1;
-       iNetworkInt = htonl(iSize);
-       if (gzwrite(pOut, (void *)&iNetworkInt, sizeof(int32)) != sizeof(int32))
-       {
-           fclose(pIn);
-           gzclose(pOut);
-           delete pBuffer;
-           return kError_WriteFile;
-       }
-       if (gzwrite(pOut, (void *)oFile.c_str(), iSize) != iSize)
-       {
-           fclose(pIn);
-           gzclose(pOut);
-           delete pBuffer;
-           return kError_WriteFile;
-       }
+       // create header
+       memset(&TarRecord,0,sizeof(tar_record));
+       strcpy(TarRecord.header.name,oFile.c_str());
+       strcpy(TarRecord.header.mode,"100644 ");
+       strcpy(TarRecord.header.uid,"   764 ");
+       strcpy(TarRecord.header.gid,"   144 ");
 
        fseek(pIn, 0, SEEK_END);
        iSize = ftell(pIn);
        fseek(pIn, 0, SEEK_SET);
+       sprintf(TarRecord.header.size,"%11o",iSize);
+       TarRecord.header.size[11]='\x20';
 
-       iNetworkInt = htonl(iSize);
-       if (gzwrite(pOut, (void *)&iNetworkInt, sizeof(int32)) != sizeof(int32))
+       time(&ltime);
+       sprintf(TarRecord.header.mtime,"%11o",ltime);
+       TarRecord.header.mtime[11]='\x20';
+
+       TarRecord.header.typeflag = '0'; // normal file
+       strcpy(TarRecord.header.magic,TMAGIC);
+
+
+       // last, create checksum
+       memcpy(TarRecord.header.chksum,CHKBLANKS,8);
+       int ctr,sum;
+       for(ctr=sum=0;ctr<sizeof(tar_record);ctr++) sum+=TarRecord.charptr[ctr];
+       sprintf(TarRecord.header.chksum,"%6o",sum);
+
+       if (gzwrite(pOut, (void *)&TarRecord, sizeof(tar_record)) != sizeof(tar_record))
        {
            fclose(pIn);
            gzclose(pOut);
            delete pBuffer;
            return kError_WriteFile;
        }
+
+
+       iPadding = (iSize/512+1)*512-iSize;
+
        for(; iSize > 0;)
        {
            iBlock = min(iSize, iBufferSize);
@@ -155,12 +185,65 @@ Error ThemeZip::CompressThemeZip(const string &oDestFile,
            iSize -= iBlock;
        }          
        fclose(pIn);
+
+       if(iPadding)
+       {
+           memset(pBuffer,0,iPadding);
+           gzwrite(pOut,pBuffer,iPadding);
+       }
    }
    delete pBuffer;
+
+   // write end-marker (zero-ed tar-record)
+   memset(&TarRecord,0,sizeof(tar_record));
+   gzwrite(pOut,(void*)&TarRecord, sizeof(tar_record));
+   // and padding (and why the hell TAR needs extra 4kb with zeroes?)
+   iSize=gztell(pOut);
+   iPadding = (iSize/512+1)*512-iSize;
+   if(iPadding)
+   {
+       memset(pBuffer,0,0x1000+iPadding);
+       gzwrite(pOut,pBuffer,0x1000+iPadding);
+   }
+
    gzclose(pOut);
 
    return kError_NoErr;
 }
+
+// some trophy from TAR code -------------------------
+
+#define ISODIGIT(Char) \
+  ((unsigned char) (Char) >= '0' && (unsigned char) (Char) <= '7')
+#define ISSPACE(Char) (__isascii (Char) && isspace (Char))
+
+
+from_oct (int digs, char *where)
+{
+  long value;
+
+  while (ISSPACE (*where))
+    {               /* skip spaces */
+      where++;
+      if (--digs <= 0)
+    return -1;      /* all blank field */
+    }
+  value = 0;
+  while (digs > 0 && ISODIGIT (*where))
+    {
+      /* Scan til nonoctal.  */
+
+      value = (value << 3) | (*where++ - '0');
+      --digs;
+    }
+
+  if (digs > 0 && *where && !ISSPACE (*where))
+    return -1;          /* ended on non-space/nul */
+
+  return value;
+}
+
+// ---------------------------- TAR code ends
 
 Error ThemeZip::DecompressThemeZip(const string &oSrcFile,
                                    const string &oDestPath)
@@ -168,9 +251,10 @@ Error ThemeZip::DecompressThemeZip(const string &oSrcFile,
    FILE   *pOut;
    gzFile  pIn;
    char   *pBuffer;
-   int     iNetworkInt, iWrite, iRead, iSize, iBlock;
-   int     iMajor, iMinor, iNumFiles, i;
+   int     iWrite, iRead, iSize, iBlock, iPadding;
+   int     fDone;
    string  oFile;
+   tar_record TarRecord;
 
    if (oSrcFile.length() == 0)
       return kError_FileNotFound;
@@ -181,55 +265,39 @@ Error ThemeZip::DecompressThemeZip(const string &oSrcFile,
    if (pIn == NULL)
        return kError_FileNotFound;
 
-   if (gzread(pIn, &iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pIn);
-       return kError_ReadFile;
-   }
-   iNumFiles = ntohl(iNetworkInt); 
-
-   if (gzread(pIn, &iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pIn);
-       return kError_ReadFile;
-   }
-   iMajor = ntohl(iNetworkInt); 
-
-   if (gzread(pIn, &iNetworkInt, sizeof(int32)) != sizeof(int32))
-   {
-       gzclose(pIn);
-       return kError_ReadFile;
-   }
-   iMinor = ntohl(iNetworkInt); 
-
-   if (iMajor != iMajorVersion || iMinor != iMinorVersion)
-   {
-       gzclose(pIn);
-       return kError_InvalidVersion;
-   }
-
    pBuffer = new char[iBufferSize];
-   for(i = 0; i < iNumFiles; i++)
-   {
-       char *pFileBuffer;
+   fDone = FALSE;
 
-       if (gzread(pIn, &iNetworkInt, sizeof(int32)) != sizeof(int32))
+   while(!fDone)
+   {
+       if (gzread(pIn, &TarRecord, sizeof(tar_record)) != sizeof(tar_record))
        {
            gzclose(pIn);
            return kError_ReadFile;
        }
-       iSize = ntohl(iNetworkInt); 
-
-       pFileBuffer = new char[iSize];
-       if (gzread(pIn, pFileBuffer, iSize) != iSize)
+       // check if we are done
+       int ctr,sum;
+       for(ctr=sum=0; ctr<sizeof(tar_record); ctr++) sum+=TarRecord.charptr[ctr];
+       if(sum==0)
+       {
+           // empty record detected, done
+           fDone = TRUE;
+           break;
+       }
+       // check if checksum is still OK
+       int our_sum = from_oct(6,TarRecord.header.chksum);
+       memcpy(TarRecord.header.chksum,CHKBLANKS,8);
+       for(ctr=sum=0;ctr<sizeof(tar_record);ctr++) sum+=TarRecord.charptr[ctr];
+       if(sum!=our_sum)
        {
            gzclose(pIn);
            delete pBuffer;
-           return kError_ReadFile;
+           CleanupThemeZip();
+           return kError_NoDataAvail;
        }
-       oFile = oDestPath + string(DIR_MARKER_STR) + string(pFileBuffer);
-       delete pFileBuffer;
+       // yes, can proceed
 
+       oFile = oDestPath + string(DIR_MARKER_STR) + string(TarRecord.header.name);
        pOut = fopen(oFile.c_str(), "wb");
        if (pOut == NULL)
        {
@@ -239,14 +307,8 @@ Error ThemeZip::DecompressThemeZip(const string &oSrcFile,
        }
        m_oCreatedFiles.push_back(oFile);
 
-       if (gzread(pIn, &iNetworkInt, sizeof(int32)) != sizeof(int32))
-       {
-           fclose(pOut);
-           gzclose(pIn);
-           CleanupThemeZip();
-           return kError_ReadFile;
-       }
-       iSize = ntohl(iNetworkInt); 
+       iSize = from_oct(11,TarRecord.header.size); 
+       iPadding = (iSize/512+1)*512-iSize;
 
        for(; iSize > 0;)
        {
@@ -273,6 +335,12 @@ Error ThemeZip::DecompressThemeZip(const string &oSrcFile,
            iSize -= iBlock;
        }          
        fclose(pOut);
+
+       if(iPadding)
+       {
+           gzseek(pIn, iPadding, SEEK_CUR);
+       }
+
    }
    delete pBuffer;
    gzclose(pIn);

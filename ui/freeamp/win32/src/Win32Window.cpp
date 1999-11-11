54c55388c1922fc94dc791eecf128717740f3e8a
robert <robert>
1999-11-11 01:22:15 +0000

    Fixed the mouse-over-elrod-is-being-sneaky-bug. Again.

--- ui/freeamp/win32/src/Win32Window.cpp
+++ ui/freeamp/win32/src/Win32Window.cpp
@@ -19,7 +19,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-   $Id: Win32Window.cpp,v 1.13 1999/11/10 20:57:41 robert Exp $
+   $Id: Win32Window.cpp,v 1.14 1999/11/11 01:22:15 robert Exp $
 ____________________________________________________________________________*/ 
 
 // The debugger can't handle symbols more than 255 characters long.
@@ -750,13 +750,16 @@ void Win32Window::MouseLeaveCheck(void)
     Rect  oRect;
     POINT sPos;
     Pos   oPos;
+    HDC   hDc;
     
     GetWindowPosition(oRect);
     GetCursorPos(&sPos);
     oPos.x = sPos.x;
     oPos.y = sPos.y;
+    ScreenToClient(m_hWnd, &sPos);
     
-    if (!oRect.IsPosInRect(oPos))
+    hDc = GetDC(m_hWnd);
+    if (!oRect.IsPosInRect(oPos) || !PtVisible(hDc, sPos.x, sPos.y))
     {
         if (m_bMouseInWindow)
             MouseHasLeftWindow();
@@ -764,4 +767,6 @@ void Win32Window::MouseLeaveCheck(void)
     }
     else
         m_bMouseInWindow = true;
+        
+    ReleaseDC(m_hWnd, hDc);
 }

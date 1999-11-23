6fcbc3cf218d79290efc940346f19bb3feed07d2
elrod <elrod>
1999-11-23 19:14:16 +0000

    * Somehow the resource defines for the checked and unchecked BMPs were   removed.

--- ui/freeamp/win32/res/resource.h
+++ ui/freeamp/win32/res/resource.h
@@ -172,7 +172,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        197
+#define _APS_NEXT_RESOURCE_VALUE        199
 #define _APS_NEXT_COMMAND_VALUE         40013
 #define _APS_NEXT_CONTROL_VALUE         1056
 #define _APS_NEXT_SYMED_VALUE           101

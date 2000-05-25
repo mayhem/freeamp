8469310fb3daf69f19f6ad9b768b9f92427478e0
elrod <elrod>
2000-05-25 10:59:47 +0000

    * removed use of constant 255 and replaced it with _MAX_PATH.

--- ui/freeamp/src/FreeAmpTheme.cpp
+++ ui/freeamp/src/FreeAmpTheme.cpp
@@ -19,7 +19,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
         
-   $Id: FreeAmpTheme.cpp,v 1.116 2000/05/24 16:28:49 elrod Exp $
+   $Id: FreeAmpTheme.cpp,v 1.117 2000/05/25 10:59:47 elrod Exp $
 ____________________________________________________________________________*/
 
 // The debugger can't handle symbols more than 255 characters long.
@@ -1411,14 +1411,15 @@ void FreeAmpTheme::InitWindow(void)
 void FreeAmpTheme::ReloadTheme(void)
 {
     char    *szTemp;
-    uint32   iLen = 255;
+    uint32   iLen = _MAX_PATH;
     string   oThemePath(""), oThemeFile("theme.xml");
     Error    eRet;
     struct  _stat buf;
 
     szTemp = new char[iLen];
 
-    m_pContext->prefs->GetPrefString(kThemePathPref, szTemp, &iLen);
+    eRet = m_pContext->prefs->GetPrefString(kThemePathPref, szTemp, &iLen);
+
     if (_stat(szTemp, &buf) < 0 && strlen(szTemp) > 0)
     {
        // If the theme doesn't exist, let's try to prepend the install/theme dir

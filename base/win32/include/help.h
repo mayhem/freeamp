a8f31c7f2e52c2c2557f7dfb6eb17bb1e2c23bcf
robert <robert>
2000-06-12 13:25:39 +0000

    Adding new HTML help entry points

--- base/win32/include/help.h
+++ base/win32/include/help.h
@@ -18,7 +18,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-   $Id: help.h,v 1.4 2000/06/07 13:01:08 elrod Exp $
+   $Id: help.h,v 1.5 2000/06/12 13:25:39 robert Exp $
 ____________________________________________________________________________*/ 
 
 #ifndef INCLUDED_HELP_H
@@ -26,29 +26,18 @@ ____________________________________________________________________________*/
 
 #define HELP_FILE BRANDING_HELP_FILE
 
-//Main Dialog by default in Help_Finder
-#define FreeAmp_Main_Window	0x0	
-
-//Music Browser
-#define Music_Browser 1
-//Actual name of dialog for drive selection unknown
-//Temporary name used to allocate ID number.
-#define Music_Browser_Drive_Selection 10
-
-//Download Manager
-#define Download_Manager 2
-
-//Preferences Dialog
-#define Preferences_General 3
-#define Preferences_Themes 4
-#define Preferences_Streaming 5
-#define Preferences_Update 6
-#define Preferences_Plugins 7
-#define Preferences_Advanced 8
-#define Preferences_About 9
-#define Preferences_Directory 12
-
-#define Edit_Info 11
-
+#define FreeAmp_Main_Window           "help.html"
+#define Music_Browser                 "music_browser_entry.html"
+#define Music_Browser_Drive_Selection "drive_selection_entry.html"
+#define Download_Manager              "download_manager_entry.html"
+#define Preferences_General           "preferences_general_entry.thml"
+#define Preferences_Themes            "preferences_themes_entry.html"
+#define Preferences_Streaming         "preferences_streaming_entry.html"
+#define Preferences_Update            "preferences_update_entry.html"
+#define Preferences_Plugins           "preferences_plugins_entry.html"
+#define Preferences_Advanced          "preferences_advanced_entry.html"
+#define Preferences_About             "preferences_about_entry.html"
+#define Preferences_Directory         "preferences_directory_entry.html"
+#define Edit_Info                     "edit_info_entry.html"
 
 #endif

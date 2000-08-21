955a87ade859966e62ed4d268b9e59e3fb9c108a
ijr <ijr>
2000-08-21 12:41:35 +0000

    - Successfully generating sigs wasn't deleting the track from the   'to be signatured' set

--- base/src/musiccatalog.cpp
+++ base/src/musiccatalog.cpp
@@ -18,7 +18,7 @@
         along with this program; if not, write to the Free Software
         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-        $Id: musiccatalog.cpp,v 1.76 2000/08/21 08:05:22 ijr Exp $
+        $Id: musiccatalog.cpp,v 1.77 2000/08/21 12:41:35 ijr Exp $
 ____________________________________________________________________________*/
 
 // The debugger can't handle symbols more than 255 characters long.
@@ -1311,6 +1311,8 @@ Error MusicCatalog::AcceptEvent(Event *e)
             string url = curl;
             delete [] curl;
        
+			m_sigs->erase(url);
+
             MetaData *data = ReadMetaDataFromDatabase(url.c_str());
             if (!data)
                 data = new MetaData;
@@ -1342,7 +1344,7 @@ Error MusicCatalog::AcceptEvent(Event *e)
             m_context->aps->APSLookupSignature(sig, GUID);
 
             if (GUID != "") {
-                m_sigs->erase(asge->Url());
+				m_sigs->erase(asge->Url());
 
                 uint32 length = asge->Url().size() + 20;
                 char *curl = new char[length];
@@ -1351,6 +1353,7 @@ Error MusicCatalog::AcceptEvent(Event *e)
                 string url = curl;
                 delete [] curl;
 
+				m_sigs->erase(url);
                 MetaData *data = ReadMetaDataFromDatabase(url.c_str());
 
                 if (!data) 

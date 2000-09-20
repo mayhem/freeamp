36015888f07f4d0100ec92646dc5a32eac29d785
robert <robert>
2000-09-20 12:59:13 +0000

    Fixed bug 994 (crash on load of playlist due to missing file)

--- io/local/localfileinput.cpp
+++ io/local/localfileinput.cpp
@@ -18,7 +18,7 @@
         along with this program; if not, write to the Free Software
         Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
         
-        $Id: localfileinput.cpp,v 1.34 2000/07/31 19:51:39 ijr Exp $
+        $Id: localfileinput.cpp,v 1.35 2000/09/20 12:59:13 robert Exp $
 ____________________________________________________________________________*/
 
 /* system headers */
@@ -240,7 +240,8 @@ Error LocalFileInput::Seek(int32 &iRet, int32 iPos, int32 iFrom)
 
 void LocalFileInput::Clear(void) 
 { 
-    fseek(m_fpFile, SEEK_SET, 0); 
+	if (m_fpFile)
+       fseek(m_fpFile, SEEK_SET, 0); 
     m_pOutputBuffer->Clear();
     PipelineUnit::Clear();
 }

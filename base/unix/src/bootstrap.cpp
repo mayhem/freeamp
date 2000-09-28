d85d577a77057c0e852bfeaa8a41b5ae63353c44
ijr <ijr>
2000-09-28 12:11:00 +0000

    - doh!

--- base/unix/src/bootstrap.cpp
+++ base/unix/src/bootstrap.cpp
@@ -18,7 +18,7 @@
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 	
-	$Id: bootstrap.cpp,v 1.32 2000/09/28 11:33:09 ijr Exp $
+	$Id: bootstrap.cpp,v 1.33 2000/09/28 12:11:00 ijr Exp $
 ____________________________________________________________________________*/
 
 #include "config.h"
@@ -65,7 +65,7 @@ static void PrintMutexDebugInfo(int sig)
 #endif
 
 #if ( (defined(__GNU_LIBRARY__) && !defined(_SEM_SEMUN_UNDEFINED)) \
-    || (defined(__FreeBSD__)) || defined(irix)))
+    || (defined(__FreeBSD__)) || (defined(irix)))
 #else
 union semun 
 {
